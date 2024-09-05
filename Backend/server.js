b51b11b03f6e210167443edb8e5020645f00b419
Raj Gupta <code.raj11903@gmail.com>
2024-09-05 12:21:22 +0530

    installation done and readme Updated

---
+++ Backend/server.js
@@ -0,0 +1,29 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./database/connectDB.js";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
+dotenv.config();
+
+connectDB();
+
+
+const PORT = process.env.PORT || 4000;
+const __dirname = path.resolve();
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Middlewares
+app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(cookieParser());
+
+// Routes
+// app.use("/api/users", userRoutes);
+
+server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
