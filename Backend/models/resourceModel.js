b913aea4766b9186b9427be3fe885a236faab8c3
Raj Gupta <code.raj11903@gmail.com>
2024-09-20 12:41:08 +0530

    Resource Model updated

--- Backend/models/resourceModel.js
+++ Backend/models/resourceModel.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
 const ResourceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Resource name is required"],
+  },
+  location: {
+    type: String,
+    required: [true, "Resource location is required"],
+  },
   type: {
     type: String,
     required: [true, "Resource type is required"],
@@ -9,8 +17,14 @@ const ResourceSchema = new mongoose.Schema({
     type: String,
     required: [true, "Resource description is required"],
   },
-  availability: { type: Boolean, default: true },
-  department_id: { type: mongoose.Schema.Types.ObjectId, ref: "Department" },
+  availability: { 
+    type: Boolean, 
+    default: true 
+  },
+  department_id: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Department" 
+  },
   institute_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Institute",
@@ -20,7 +34,10 @@ const ResourceSchema = new mongoose.Schema({
     type: Number,
     required: [true, "Resource price per day is required"],
   },
-  image_url: { type: String },
+  image_url: { 
+    type: String, 
+    required: [true, "Resource image URL is required"], 
+  },
   createdAt: {
     type: Date,
     default: Date.now
